9daa41509b1bd015a34090661f3bdeb64474b4ed
Sherlock-HolmesJM <sherllyj191@gmail.com>
2021-11-30 06:25:24 +0100

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import { Switch, Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
 import "./App.css";
-import RegisterForm from "./components/registerForm";
-import LoginForm from "./components/loginForm";
 
 function App() {
 	return (
 		<div className="App">
-			<Switch>
-				<Route path="/register" component={RegisterForm} />
-				<Route path="/" component={LoginForm} />
-			</Switch>
+			<Switch></Switch>
 		</div>
 	);
 }
